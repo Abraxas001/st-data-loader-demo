57a8e66f5a19f09d507c26043da2e87383c04177
Shivam Gupta <86199904+Abraxas001@users.noreply.github.com>
2024-03-01 14:10:41 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,12 +4,14 @@ import json
 import csv
 from snowflake.snowpark import Session
 
-#connect to Snowflake
-with open('creds.json') as f:
-    connection_parameters = json.load(f)  
-session = Session.builder.configs(connection_parameters).create()
 
-file = st.file_uploader("Drop your CSV here to load to Snowflake", type={"csv"})
-if file is not None:
-    file_df = pd.read_csv(file, engine='python', sep=',',  quotechar='"')
-    snowparkDf=session.write_pandas(file_df,file.name,auto_create_table = True, overwrite=True)
+st.write('Hello World!')
+#connect to Snowflake
+#with open('creds.json') as f:
+#    connection_parameters = json.load(f)  
+#session = Session.builder.configs(connection_parameters).create()
+
+#file = st.file_uploader("Drop your CSV here to load to Snowflake", type={"csv"})
+#if file is not None:
+#    file_df = pd.read_csv(file, engine='python', sep=',',  quotechar='"')
+#    snowparkDf=session.write_pandas(file_df,file.name,auto_create_table = True, overwrite=True)
