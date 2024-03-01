58ec830e60161efd38f585968ff84a363acadeb9
Shivam Gupta <86199904+Abraxas001@users.noreply.github.com>
2024-03-01 15:09:11 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,13 +5,13 @@ import streamlit as st
 #from snowflake.snowpark import Session
 
 
-st.write('Hello World!')
+#st.write('Hello World!')
 #connect to Snowflake
-#with open('creds.json') as f:
-#    connection_parameters = json.load(f)  
-#session = Session.builder.configs(connection_parameters).create()
+with open('creds.json') as f:
+    connection_parameters = json.load(f)  
+session = Session.builder.configs(connection_parameters).create()
 
-#file = st.file_uploader("Drop your CSV here to load to Snowflake", type={"csv"})
-#if file is not None:
-#    file_df = pd.read_csv(file, engine='python', sep=',',  quotechar='"')
-#    snowparkDf=session.write_pandas(file_df,file.name,auto_create_table = True, overwrite=True)
+file = st.file_uploader("Drop your CSV here to load to Snowflake", type={"csv"})
+if file is not None:
+    file_df = pd.read_csv(file, engine='python', sep=',',  quotechar='"')
+    snowparkDf=session.write_pandas(file_df,file.name,auto_create_table = True, overwrite=True)
