bbf3f472642e06aed935319e0c1eb0f8540ff0bc
Shivam Gupta <86199904+Abraxas001@users.noreply.github.com>
2024-03-04 17:35:18 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -48,8 +48,8 @@ conn = snowflake.connector.connect(
 
 file = st.file_uploader("Drop your CSV here to load to Snowflake", type={"csv"})
 #if file is not None:
-    file_df = pd.read_csv(file, engine='python', sep=',',  quotechar='"')
-    snowparkDf=write_pandas(conn, file_df,file.name,auto_create_table = True, overwrite=True)
+file_df = pd.read_csv(file, engine='python', sep=',',  quotechar='"')
+snowparkDf=write_pandas(conn, file_df,file.name,auto_create_table = True, overwrite=True)
 
     #print(file_df)
 
