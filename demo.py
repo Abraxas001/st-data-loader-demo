3b9661e9751c34fce25c6488839061b528aa79c1
Shivam Gupta <86199904+Abraxas001@users.noreply.github.com>
2024-03-05 12:18:14 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -22,7 +22,7 @@ entered_username = st.text_input("Enter Username:")
 
 #st.title("User Input Demo")
 # Text input widget
-entered_password = st.text_input("Enter Password:", type="password")
+entered_password = st.text_input("Enter Password:")
 st.markdown('##')
 st.markdown('##')
 
